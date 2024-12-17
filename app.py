10bc49f1e79e9adcf1164d0a0a8c4ea06d85e75f
manu-2610 <manueldigiacomo112@gmail.com>
2024-12-17 11:42:30 +0000

    safasg

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+app.config['MYSQL_HOST'] = '138.41.20.102'
+app.config['MYSQL_PORT'] = 53306
+app.config['MYSQL_USER'] = 'ospite'
+app.config['MYSQL_PASSWORD'] = 'ospite'
+app.config['MYSQL_DB'] = 'w3school'
+mysql = MySQL(app)
 
-app.run()
+cursor = mysql.connection.cursor()
+sql = "SELECT * from products"
+cursor.execute(sql)
+prodotti = cursor.fetchall()
+
+@app.route("/")
+def home():
+    return render_template("home.html", titolo="Home")
+
+@app.route("/products")
+def products():
+    return render_template("products.html", titolo="Prodotti", prodotti = prodotti)
+
+app.run(debug=True)
